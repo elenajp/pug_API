cbe2e935803616be29755ec19f9dd880eb8b825a
elena perez <ejperezk@gmail.com>
2021-08-25 17:04:46 +0200

    added app.route for a get request which can be viewed with the /sharks endpoint in browser

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
-from db import db
+from database import result_list
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# this prevents a warning message when running the app
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# # this is the location of the database, can be local or remote
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
 
@@ -12,25 +16,23 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
+# @app.route('/<string:page_name>/')
+# def render_static(page_name):
+#     return render_template('%s.html' % page_name)
 
 
-# @app.route("/about")
-# def about():
-#     return render_template('about.html')
-
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 # @app.route("/docs")
 # def docs():
  #    return render_template('docs.html')
 
-@app.route('/upload', method=['POST'])
-def upload():
-    image = request.files['image']
-    if not image:
-        return 'no image uploaded', 400
+
+@app.route('/sharks', methods=['GET'])
+def return_sharks():
+    return jsonify({'result_list': result_list})
 
 
 if __name__ == '__main__':
